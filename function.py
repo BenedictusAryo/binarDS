58095e133e3d5a5ae6d33d3f75e305278547d4be
BenedictusAryo <benedictusaryo@outlook.com>
2022-11-03 20:07:24 +0700

    tambah fungsi

---
+++ function.py
@@ -0,0 +1,12 @@
+def tambah(angka_1, angka_2):
+    """Fungsi menambah 2 angka"""
+    return angka_1 + angka_2
+
+
+print("masukkan angka 1: ")
+angka_1 = float(input())
+print("masukkan angka 2: ")
+angka_2 = float(input())
+
+hasil = tambah(angka_1, angka_2)
+print(f"Hasilnya: {hasil}")
